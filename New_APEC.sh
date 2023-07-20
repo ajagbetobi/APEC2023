073b3545a65c18d825ae0fe7b788cb8eba8d059f
Stephen Ajagbe <aostephen1000@gmail.com>
2023-07-19 21:34:35 -0400

    Comment and Edit New_APEC.sh

--- New_APEC.sh
+++ New_APEC.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 #
-# Copying generic Infos.dat from installation directory
-# If Infos.dat already exists, the script terminates to avoid overwriting
+# Check if Infos.dat already exists in current directory. If so, terminate to avoid overwriting.
 #
 if [ -f Infos.dat ]; then
    echo ""
-   echo " ARM fatal error - Infos.dat is existing"
-   echo " The new project must start in a folder with no other ARM projects"
-   echo " Just create a suitable directory, move New.sh there and run it"
+   echo " ARM fatal error - Infos.dat exists."
+   echo " The new project must start in a folder with no other ARM projects."
+   echo " Create a suitable directory, move New_APEC.sh there and run it."
    echo ""
-   echo " New.sh is terminating"
+   echo " New_APEC.sh is exists..C_APEC."
    exit 0
 fi
 echo ""
@@ -40,38 +39,47 @@ echo ""
 echo ""
 echo ""
 
-#
-# Defining the path of the APEC code
-#
+#Load/Call on applications APEC procedure will use.
 module load openmolcas
 module load gromacs
+
+# Define the path of the APEC code installation directory and copy generic Infos.dat file from there to current APEC procedure directory.
+
 camino="/userapp/APEC_Spr2023/New_APEC/template"
 cp $camino/Infos.dat .
 
 #
-# Asking the name of the project
+# Ask for the name of the project
 #
 echo " What is the name of the project (the name MUST not start with a number)??"
 read Project
 echo ""
 
+#Check if Step_0 (First APEC Iteration) already exists, terminate if so.
 if [ -d Step_0 ]; then
       echo " Folder \"Step_0\" found! Something is wrong ..."
       echo " Terminating ..."
       exit 0
       echo ""
 fi
+
+#Make Step_0
+
 mkdir Step_0
+
 #
-# Finding all the PDB files in the current folder
+# Find all the PDB files in the current folder.
 #
 i=1
 ext=pdb
+
+# Go through each pdb file in the current directory, add them to a filelist
 for f in *.$ext; do
     filelist[$i]=$f
     i=$(($i+1))
 done
 i=$(($i-1))
+
 #
 # Variable len counts how many PDB files are in the folder, len+1 is required for ending
 # the while loop smoothly. Here the user is asked to select which PDB file has to be used
@@ -112,14 +120,14 @@ fi
 cd Step_0/
 
 #
-# Counts the total charge of the protein without the chromophore
+# Count the total charge of the protein without the chromophore.
 #
 plusc=`awk '{ print $4 " " $5 " " $6 }' ${Project}.pdb | uniq | awk '{ if ( $1 == "LYS" || $1 == "HIS" || $1 == "ARG" || $1 == "NA" ) print $1 }' | wc -l`
 minusc=`awk '{ print $4 " " $5 " " $6 }' ${Project}.pdb | uniq | awk '{ if ( $1 == "ASP" || $1 == "GLU" || $1 == "CL" || $1 == "ACI" || $1 == "TYO" ) print $1 }' | wc -l`
 totcarica=$(($plusc-$minusc))
 
 #
-# Checking if Infos.dat was copied
+# Check if Infos.dat was copied. Terminate otherwise.
 #
 if [ -z Infos.dat ]; then
    echo " ARM fatal error! - Infos.dat not found"
@@ -131,14 +139,21 @@ if [ -z Infos.dat ]; then
    cd ..
    rm -r Step_0
    exit 0
+
+#If it was copied, update the Project key in the Infos.dat file with the value of the project name given by the user.
 else
    echo "Project $Project" >> Infos.dat
+
+#Get APEC Installation directory from Infos file and save as templatedir.
    templatedir=`grep "Template" Infos.dat | awk '{ print $2 }'`
 fi
 #
-# Retriving amber99sb force field for tinker
+# Retrieve amber99sb force field parameters from Installation Directory for Tinker.
 #
 cp $templatedir/amber99sb.prm amber99sb.prm
+
+# Update Infos.dat file with the following key value pairs.
+
 echo "AMBER amber99sb" >> Infos.dat
 echo "Parameters amber99sb" >> Infos.dat
 echo "CurrCalc Start" >> Infos.dat
@@ -146,8 +161,7 @@ echo "CurrCalc Start" >> Infos.dat
 cp $templatedir/smooth_restart.sh .
 
 #
-# Checking for multiple chains in the PDB file. If so, it writes it in Infos.dat 
-# for future use
+# Checking for multiple chains in the PDB file. Write this in Infos.dat for future use.
 #
 grep 'ATOM' ${Project}.pdb > atoms
 nchain=`grep 'TER' ${Project}.pdb | wc -l`
@@ -166,8 +180,7 @@ else
 fi
 
 #
-# Writing chain info in Infos.dat and running the appropriate pdbxyz command
-#
+# Write chain info in Infos.dat
 if [[ $multchain == "yes" ]]; then
    echo "MultChain YES" >> Infos.dat
    echo "LastRes ${lastres[@]}" >> Infos.dat 
@@ -176,7 +189,8 @@ else
    echo "MultChain NO" >> Infos.dat
 fi
 #
-# Checking if a list of residues belonging to the retinal cavity is provided
+# Check if a list of residues belonging to the retinal cavity is provided. 
+# If it is copy to current directory and update Infos.dat file.
 #
 if [[ -f ../cavity ]]; then
    answer=b
@@ -221,7 +235,7 @@ if [[ $modelchk == YES ]]; then
 fi
 
 #
-# Coping the xyz file of the chromophore
+# Copy the xyz file of the chromophore
 #
 chromophore="CHR_chain"
 
@@ -240,7 +254,7 @@ else
 fi
 
 #
-# Defining the total charge of the chromophore
+# Define the total charge of the chromophore
 #
 chr=20
 while [[ $chr -ne -4 && $chr -ne -3 && $chr -ne -2 && $chr -ne -1 && $chr -ne 0 && $chr -ne 1 && $chr -ne 2 && $chr -ne 3 && $chr -ne 4 ]]; do
@@ -253,7 +267,7 @@ while [[ $chr -ne -4 && $chr -ne -3 && $chr -ne -2 && $chr -ne -1 && $chr -ne 0 
 done
 
 #
-# Adding Chromophore to the protein pdb before generating the mutations
+# Add Chromophore atoms to the protein pdb before generating the mutations
 #
 grep "ATOM  " $Project.pdb > ATOMS
 lastatom=`tail -n1 ATOMS | awk '{ print $2 }'`
@@ -262,6 +276,9 @@ chratoms=`head -n1 ../$chromophore.xyz`
 
 head -n $(($chratoms+2)) ../$chromophore.xyz | tail -n $chratoms > temp1
 awk '{ print $2,"   ",$3,"   ",$4,"   ",$5}' temp1 > xyz
+
+
+#Fortran Section
 
 cat > format.f << YOE
       Program format
@@ -282,6 +299,7 @@ YOE
 gfortran format.f -o format.x
 ./format.x
 
+#Append Heteroatoms in Project pdb to Water and Chromophore Hetero atoms files
 grep "HETATM" $Project.pdb > HETATM_wat
 grep "HETATM" $Project.pdb >> HETATM_CHR
 sed -i "/HETATM/d" $Project.pdb
@@ -289,7 +307,7 @@ cat HETATM_CHR >> $Project.pdb
 
 #
 # If seqmut is found, the mutation routine is started, given that either Modeller or SCWRL4 is available
-# Such information is retrieved from Infos.dat
+# This information is gotten from Infos.dat
 #
 if [[ -f seqmut ]]; then
    scwrlchk=`grep "SCWRL4" Infos.dat | awk '{ print $2 }'`
@@ -303,7 +321,10 @@ if [[ -f seqmut ]]; then
       echo ""
       exit 0
    fi
+#Search for the mutation error ID in an error file.
+
    errid=`grep 'mutate_errid' arm.err | awk '{ print $2 }'`
+#If it isn't found, output this.
    if [[ $errid -eq 0 ]]; then
       echo " The following mutations has been inserted:"
       cat seqmut
@@ -332,15 +353,15 @@ cat HETATM_wat >> $Project.pdb
 rm ATOMS temp1 xyz HETATM_wat HETATM_CHR format.x format.f
 
 #
-# Recomputinhg the total charge of the protein after generating the mutations
+# Recompute the total charge of the protein after generating the mutations.
 #
 plusc=`awk '{ print $4 " " $5 " " $6 }' ${Project}.pdb | uniq | awk '{ if ( $1 == "LYS" || $1 == "HIS" || $1 == "ARG" || $1 == "NA" ) print $1 }' | wc -l`
 minusc=`awk '{ print $4 " " $5 " " $6 }' ${Project}.pdb | uniq | awk '{ if ( $1 == "ASP" || $1 == "GLU" || $1 == "CL" || $1 == "ACI" || $1 == "TYO" ) print $1 }' | wc -l`
 totcarica=$(($plusc-$minusc))
 
 #
-# Converting the geometry of the chromophore from xyz to pdb for future 
-# conversion to gromacs
+# Convert the geometry of the chromophore from xyz to pdb for future 
+# conversion to gromacs.
 #
 mkdir Chromophore
 cp ../$chromophore.xyz Chromophore
@@ -348,6 +369,9 @@ cd Chromophore
 
 numatm=`head -n1 $chromophore.xyz | awk '{ print $1 }'`
 end=`grep -n "End\|end\|END" $chromophore.xyz | cut -f1 -d:`
+
+
+#Fortran Section
 
 cat > write_pdb.f << YOE
       Program write_pdb
@@ -400,11 +424,15 @@ cp $templatedir/ASEC/update_infos.sh .
 ./update_infos.sh "Init_Charge" $carga Infos.dat
 ./update_infos.sh "Chromo_Charge" $chr Infos.dat
 cp $templatedir/NewStep.sh .
+
+#Copy Next_Script from TemplateDir to Step_0 folder
+cp $templatedir/Next_Script.sh Step_0/
+
 echo ""
 echo ""
 echo " ****************************************************"
 echo ""
-echo " Folder Step_0 created! cd Step_0/, then ./NewStep.sh"
+echo " Step_0 Folder created! cd Step_0/, then ./NewStep.sh"
 echo ""
 echo " ****************************************************"
 echo ""
