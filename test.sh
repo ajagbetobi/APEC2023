f7a646ff126057573809bc44dd26d3b1005e4679
Stephen Ajagbe <aostephen1000@gmail.com>
2023-12-05 14:37:24 -0500

    Cleaned up calcRestart.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,20 @@
-restartCalc () {
-    calctype="$1"
-    echo ${calctype}
-    return 
-}
+# If theres no folder with Backup in the Name , create backup 1.
 
-restartCalc "VDZP"
+
+isThereAbackup=$(find . -name "traj*" -type d)
+
+if [[ -z "${isThereAbackup}" ]];then 
+        mkdir traj_1
+else 
+    highestBackup=$(ls -1d *traj* | tail -1)
+    lastBackupNo=${highestBackup: -1}
+    newBackupNo="$((${lastBackupNo}+1))"
+    echo ${newBackupNo}
+    mkdir "traj_${newBackupNo}"
+fi
+
+# If there is get the last number of the name
+
+# cut it add 1
+
+# create backup with name +1
